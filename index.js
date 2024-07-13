d82c9f3f2c2d2e85df06dda292de62fc027d9e39
Suleman615 <ghlmsuleman@gmail.com>
2024-07-13 22:23:18 +0500

    script link updated

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const expression = document.getElementById('expression');
 const result = document.getElementById('result');
 const buttons = document.querySelectorAll('button[btn-name]');
@@ -7,6 +6,7 @@ let currentInput = '';
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const btnName = button.getAttribute('btn-name');
+        console.log(btnName)
         if (btnName === 'c') {
             currentInput = '';
             result.textContent = '0'
@@ -15,10 +15,14 @@ buttons.forEach(button => {
             try {
 
                 currentInput = eval(currentInput).toString();
+                result.textContent = currentInput;
+
             } catch (e) {
-                currentInput = 'wrong Expression';
+                currentInput = '';
+                result.textContent = 'Wrong Expression';
+
+                
             }
-            result.textContent = currentInput;
         } else if (btnName === 'del') {
             currentInput = currentInput.slice(0, -1);
             expression.textContent = currentInput || '0';
